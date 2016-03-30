8638e27db4256b8f7294a6347ed3c9add6e6c686
Lingxiang Xiang <lingxiang.xiang@intel.com>
2016-03-30 16:10:24 -0700

    add README; add cond_var pthread interface functions.

--- tl-pthread.c
+++ tl-pthread.c
@@ -16,6 +16,30 @@ int pthread_mutex_init(void *mutex, void *attr) {
     return tl_pthread_mutex_init(mutex, attr);
 }
 
+int pthread_cond_broadcast(void *cond) {
+    return tc_broadcast((txcond_t*)cond);
+}
+
+int pthread_cond_destroy(void *cond) {
+    return tc_destroy((txcond_t*)cond);
+}
+
+int pthread_cond_init(void *cond, const void *cond_attr) {
+    return tc_init((txcond_t*)cond);
+}
+
+int pthread_cond_signal(void *cond) {
+    return tc_signal((txcond_t*)cond);
+}
+
+int pthread_cond_timedwait(void *cond, void *mutex, const struct timespec *timeout) {
+    return tc_timedwait((txcond_t*)cond, (txlock_t*)mutex, timeout);
+}
+
+int pthread_cond_wait(void *cond, void *mutex) {
+    return tc_wait((txcond_t*)cond, (txlock_t*)mutex);
+}
+
 __attribute__((constructor(200))) 
 static void init_tl_pthread() {
     tl_replace_libpthread(1);
