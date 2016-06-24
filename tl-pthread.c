4a082563f8ddbac70a6c74c4011ec27111659642
Joe Izraelevitz <jhi1@cs.rochester.edu>
2016-06-23 23:43:15 -0700

    major rework of function dispatch for txlock, redo initialization, alloc, and free to use INITIALIZER macros, added pthreads lock

--- tl-pthread.c
+++ tl-pthread.c
@@ -1,46 +1,30 @@
 #include "txlock.h"
 
 int pthread_mutex_lock(void *mutex) {
-    return tl_pthread_mutex_lock(mutex);
+    return tl_lock(mutex);
 }
 
 int pthread_mutex_trylock(void *mutex) {
-    return tl_pthread_mutex_trylock(mutex);
+    return tl_trylock(mutex);
 }
 
 int pthread_mutex_unlock(void *mutex) {
-    return tl_pthread_mutex_unlock(mutex);
-}
-
-int pthread_mutex_init(void *mutex, void *attr) {
-    return tl_pthread_mutex_init(mutex, attr);
+    return tl_unlock(mutex);
 }
 
 int pthread_cond_broadcast(void *cond) {
     return tc_broadcast((txcond_t*)cond);
 }
 
-int pthread_cond_destroy(void *cond) {
-    return tc_destroy((txcond_t*)cond);
-}
-
-int pthread_cond_init(void *cond, const void *cond_attr) {
-    return tc_init((txcond_t*)cond);
-}
 
 int pthread_cond_signal(void *cond) {
-    return tc_signal((txcond_t*)cond);
+    return tc_signal(cond);
 }
 
 int pthread_cond_timedwait(void *cond, void *mutex, const struct timespec *timeout) {
-    return tc_timedwait((txcond_t*)cond, (txlock_t*)mutex, timeout);
+    return tc_timedwait(cond, mutex, timeout);
 }
 
 int pthread_cond_wait(void *cond, void *mutex) {
-    return tc_wait((txcond_t*)cond, (txlock_t*)mutex);
-}
-
-__attribute__((constructor(200))) 
-static void init_tl_pthread() {
-    tl_replace_libpthread(1);
+    return tc_wait(cond, mutex);
 }
--- txlock.h
+++ txlock.h
@@ -1,44 +1,36 @@
 #ifndef _TXLOCK_H_
 #define _TXLOCK_H_
 
+#include <string.h> 
 #include <time.h> // for cond vars
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct _txlock_t;
-typedef struct _txlock_t txlock_t;
+#define TXLOCK_INITIALIZER {0}
+#define TXCOND_INITIALIZER {0}
 
-int tl_alloc(txlock_t **l);
-int tl_free(txlock_t *l);
+struct _txlock_t{
+	// must be same size as pthreads for drop in replacement
+	char data[40]; 
+};
+typedef struct _txlock_t txlock_t;
 
 int tl_lock(txlock_t *l);
 int tl_trylock(txlock_t *l);
 int tl_unlock(txlock_t *l);
 
 
-struct _txcond_t;
-typedef struct _txcond_t txcond_t;
+typedef struct{
+	// must be same size as pthreads for drop in replacement
+	char data[48]; 
+} txcond_t;
 
-int tc_alloc(txcond_t **cv);
-int tc_free(txcond_t *cv);
-int tc_init(txcond_t *cv);
-int tc_destroy(txcond_t *cv);
 int tc_wait(txcond_t *cv, txlock_t *lk);
 int tc_timedwait(txcond_t *cv, txlock_t *lk, const struct timespec *abs_timeout);
 int tc_signal(txcond_t* cv);
 int tc_broadcast(txcond_t* cv);
-
-
-
-void tl_replace_libpthread(int);
-
-int tl_pthread_mutex_init(void *m, void *attr);
-int tl_pthread_mutex_lock(void *l);
-int tl_pthread_mutex_trylock(void *l);
-int tl_pthread_mutex_unlock(void *l);
-
 
 
 #ifdef __cplusplus
