3b01109778b0feee5563ce94b575387633087023
Joe Izraelevitz <jhi1@cs.rochester.edu>
2016-12-07 10:05:41 -0800

    fixx counter in tas priority

--- txlock.c
+++ txlock.c
@@ -209,18 +209,23 @@ static int tas_priority_lock_tm(tas_lock_t *lk) {
             }
         }
         if(copy.cnt < TK_MAX_DISTANCE-TK_MIN_DISTANCE){
-            __sync_fetch_and_add(&lk->cnt,1);
-            if(enter_htm(lk)==0){return 0;}
-            __sync_fetch_and_add(&lk->cnt,-1);
-            __sync_fetch_and_add(&lk->ready,1);
-            while (tatas(&lk->val, 1)){}
-            __sync_fetch_and_add(&lk->ready,-1);
-            break;
+            int tmp = __sync_fetch_and_add(&lk->cnt,1);
+			if(tmp < TK_MAX_DISTANCE-TK_MIN_DISTANCE){
+				__sync_fetch_and_add(&lk->cnt,-1);
+			}
+            else if(enter_htm(lk)==0){return 0;}
+			else{
+				__sync_fetch_and_add(&lk->cnt,-1);
+				__sync_fetch_and_add(&lk->ready,1);
+				while (tatas(&lk->val, 1)){}
+				__sync_fetch_and_add(&lk->ready,-1);
+				break;
+			}
         }
         s = spin_wait(s);
     }
   }
-  TM_STATS_SUB(my_tm_stats->cycles, RDTSC());
+  TM_STATS_SUB(my_tm_stats->cycles, rdtsc());
   return 0;
 }
 
