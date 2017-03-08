6cb1ecfa20dd025558f83bdbb78cc469a2b3ae2d
Lingxiang Xiang <lingxiang.xiang@intel.com>
2017-03-08 02:19:35 -0800

    add perf&profiling results for STAMP & Memcached

--- txutil.h
+++ txutil.h
@@ -63,8 +63,8 @@ extern tm_stats_t* tm_stats_head;      // master thread's local state
 extern __thread tm_stats_t* my_tm_stats; // thread-local stats
 extern tm_stats_t tm_stats;             // global stats, updated only when a thread exits
 
-#define TM_NO_PROFILING
-#define TM_PROFILE_RDTSC
+//#define TM_NO_PROFILING
+//#define TM_PROFILE_RDTSC
 
 #ifdef TM_NO_PROFILING
 #define TM_STATS_ADD(stat, value)
